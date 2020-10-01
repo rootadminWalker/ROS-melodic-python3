4da0411ca3792e1a72063e370d50476e961ea877
root_walker <chiioleong519@gmail.com>
2020-10-01 12:15:46 +0800

    Directory error fixed

--- install.bash
+++ install.bash
@@ -91,7 +91,7 @@ source ~/.bashrc
 echo '[INFO] Making catkin workspace'
 echo
 
-mkdir ~/catkin_ws/src
+mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws
 catkin_make -DPYTHON_EXECUTABLE=/usr/bin/python3 -DPYTHON_INCLUDE_DIR=/usr/include/python3.6m -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.6m.so
 cd ~
